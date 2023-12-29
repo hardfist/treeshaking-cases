1e6507349029b954b944997e93661dd0ea35c7d4
hardfist <yangjianzju@gmail.com>
2023-12-29 14:14:48 +0800

    chore: pure annotation

---
+++ packages/side-effects-with-complex-statement/src/util.js
@@ -0,0 +1,5 @@
+export const c = 123;
+export const d = test();
+function test(){
+  return 456;
+}
---
+++ packages/side-effects-with-pure-annotation/src/util.js
@@ -0,0 +1,5 @@
+export const c = 123;
+export const d = /*#__PURE__*/ test();
+function test(){
+  return 456;
+}
